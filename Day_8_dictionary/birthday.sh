f34730c81f5d438b519cee5297e4f5c2fe1be7db
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-07-27 23:12:23 +0530

    Changes

--- Day_8_dictionary/birthday.sh
+++ Day_8_dictionary/birthday.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 
-declare -A list
+declare -A birth
+
+
 for (( i=1; i<=50; i++ ))
 do
-	date=$((1+RANDOM%31))
-	list[$i]=$date
+    month=$((1+RANDOM%12))
+    birth[$month]=$(( ${birth[$month]}+1 ))
 done
-echo ${list[@]}
+
+for date in ${!birth[@]}
+do
+    if (( $date == 1 ))
+    then echo "Jan : ${birth[$date]}" 
+
+
+    elif (( $date == 2 ))
+    then echo "Feb : ${birth[$date]}"
+
+
+    elif (( $date == 3 ))
+    then echo "Mar : ${birth[$date]}"
+
+
+    elif (( $date == 4 ))
+    then echo "Apr : ${birth[$date]}"
+
+
+    elif (( $date == 5 ))
+    then echo "May : ${birth[$date]}"
+
+
+    elif (( $date == 6 ))
+    then echo "Jun : ${birth[$date]}"
+
+
+    elif (( $date == 7 ))
+    then echo "Jul : ${birth[$date]}" 
+
+
+    elif (( $date == 8 ))
+    then echo "Aug : ${birth[$date]}"
+
+
+    elif [ $date == 9 ]
+    then echo "Sep : ${birth[$date]}"
+
+
+    elif (( $date == 10 ))
+    then echo "Oct : ${birth[$date]}"
+
+
+    elif (( $date == 11 ))
+    then echo "Nov : ${birth[$date]}"
+
+
+    elif (( $date == 12 ))
+    then echo "Dec : ${birth[$date]}"
+    
+    fi
+
+done
